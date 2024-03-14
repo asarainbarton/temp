4ab9c5dbbebd0c2d7268ef3867a2db5590bc258a
Asa Barton <arbarton@uci.edu>
2024-03-13 19:22:24 -0700

    added sleepiness button

--- tab2/tab2.page.ts
+++ tab2/tab2.page.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton]
 })
 export class Tab2Page {
 
   constructor() {}
+
+  addSleepiness() {
+    console.log('Add sleepiness clicked');
+  }
 
 }

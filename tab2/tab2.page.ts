97a17b770bc66c424d7853601760c951a2da21b8
Asa Barton <arbarton@uci.edu>
2024-03-13 19:32:51 -0700

    added sleepiness button additional

--- tab2/tab2.page.ts
+++ tab2/tab2.page.ts
@@ -10,12 +10,26 @@ import { IonButton } from '@ionic/angular/standalone';
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton]
 })
+
 export class Tab2Page {
+  numbers = [1, 2, 3, 4, 5, 6, 7];
+  selectedNumber: number | undefined;
+  showNumbers: boolean = false;
 
   constructor() {}
 
   addSleepiness() {
-    console.log('Add sleepiness clicked');
+    // Logic to add sleepiness
+    this.showNumbers = true;
   }
 
+  selectNumber(number: number) {
+    this.selectedNumber = number;
+  }
+
+  submitNumber() {
+    console.log(this.selectedNumber);
+    this.showNumbers = false;
+    this.selectedNumber = undefined;
+  }
 }

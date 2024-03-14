3a028d74f765934e7e3057ec0fcefc07e851df5c
Asa Barton <arbarton@uci.edu>
2024-03-13 19:37:50 -0700

    added sleepiness button fixes

--- tab2/tab2.page.ts
+++ tab2/tab2.page.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { IonButton } from '@ionic/angular/standalone';
+import { NgIf, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonButton,
+    NgIf,
+    NgForOf
+  ]
 })
-
 export class Tab2Page {
   numbers = [1, 2, 3, 4, 5, 6, 7];
   selectedNumber: number | undefined;
